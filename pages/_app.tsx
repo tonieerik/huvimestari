df0f598407d0d45984b71bbe9d044ef5a505c38d
Toni Leppänen <toni.leppanen@gmail.com>
2022-04-06 15:38:07 +0300

    Google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import { useEffect } from "react";
 import { AppProps } from "next/app";
 import Head from "next/head";
-import Script from 'next/script'
+import { useRouter } from "next/router"
+import Script from "next/script"
+import * as ga from "../lib/ga"
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageview(url)
+    }
+    
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,22 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
       <body>
